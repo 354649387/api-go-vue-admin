a4456e56925952f51ef1cb02348df88e84e48b20
minlang <minlang@163.com>
2021-08-10 16:27:08 +0800

    管理员更新，分页功能

--- controller/admin/handler.go
+++ controller/admin/handler.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"strconv"
 )
 
 var Db *sqlx.DB
@@ -32,7 +33,27 @@ func adminList(c *gin.Context) {
 	//创建一个切片存放一条条的结构体
 	var admins []Admin
 
-	rows, _ := Db.Query("select * from admin")
+	//第几页
+	page := c.Query("page")
+	//每页显示多少
+	pageSize := c.Query("pageSize")
+
+	//字符串转成数字类型
+	page1, _ := strconv.Atoi(page)
+	pageSize1, _ := strconv.Atoi(pageSize)
+
+	offsetNum := (page1 - 1) * pageSize1
+
+	//总条数
+	var total int64
+
+	err := Db.QueryRow("select count(*) from admin").Scan(&total)
+
+	if err != nil {
+		fmt.Println("获取总条数失败")
+	}
+
+	rows, _ := Db.Query("select * from admin limit ?,?", offsetNum, pageSize1)
 
 	//遍历
 	var admin Admin
@@ -45,12 +66,32 @@ func adminList(c *gin.Context) {
 
 	}
 
-	c.JSON(200, admins)
+	c.JSON(200, gin.H{"admins": admins, "total": total})
 
 }
 
 func adminAdd(c *gin.Context) {
 
-	c.JSON(200, "adminAdd")
+	username := c.Query("username")
+
+	password := c.Query("password")
+
+	res, err := Db.Exec("insert into admin(username,password) values(?,?)", username, password)
+
+	if err != nil {
+		fmt.Println("insert into admin failed", err)
+	}
+
+	rows, err := res.RowsAffected()
+
+	if rows > 0 {
+
+		c.JSON(200, "success")
+
+	} else {
+
+		c.JSON(201, err)
+
+	}
 
 }
